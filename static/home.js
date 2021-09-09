31eed619833bb146efc32bfdcd08e6c0d59c849c
JiHye Lee <40597729+jihye0320@users.noreply.github.com>
2021-09-09 14:45:21 +0900

    via JH

--- static/home.js
+++ static/home.js
@@ -5,24 +5,42 @@ function btnClick() {
         type: "POST",
         url: "/inputText",
         data: {data:inputText},
+        async:true,
         success: function(response){
            console.log(response)
            glo_key = response['keyword']
            glo_count = response['count']
            glo_questions = response['questions']
-        },
+        }
+        ,
         beforeSend:function(){
             $('.wrap-loading').removeClass('display-none');
         },
         complete:function(){
             $('.wrap-loading').addClass('display-none');
+            $("#head").empty()
+            $("#head").append(`ATLAS 인공지능이 키워드를 추출했습니다.<br>아래 우측 박스에 문제로 만들 키워드를 선택하세요.`)
+            $("#resultWindow").empty()
+            $("#resultWindow").append(`
+            <thead>
+                <tr>
+                <th>키워드</th><th>선택</th>
+                </tr>
+            </thead>`)
+                for (let i =0; i< glo_key.length; i++) {
+                    let result= glo_key[i];
+                    $("#resultWindow").append(`
+                    <tbody>
+                    <tr>
+                    <td>${result}</td>
+                    <td><input type="checkbox" name= chkbox></td>
+                    </tr>
+                    </tbody>`)}
         }
     })
 }
 
 function btnClick1() {
-   $("#head").empty()
-   $("#head").append(`ATLAS 인공지능이 키워드를 추출했습니다.<br>아래 우측 박스에 문제로 만들 키워드를 선택하세요.`)
    $("#resultWindow").empty()
    $("#resultWindow").append(`
    <thead>
@@ -41,7 +59,7 @@ function btnClick1() {
         </tbody>`)}
  }
 
- function checkByte(obj){
+function checkByte(obj){
     const maxByte = 3000; //최대 3000바이트
     const text_val = obj.value; //입력한 문자
     const text_len = text_val.length; //입력한 문자수
@@ -69,12 +87,15 @@ function btnClick1() {
         }
 }
 
- function btnClick2(){
-     $("#resultWindow").prop("checked", true);
-     $("#resultWindow").prop("checked", false);
+function btnClick2(){
+//     $("#resultWindow").prop("checked", true);
+//     $("#resultWindow").prop("checked", false);
      const inputText = $("#inputText").val();
      console.log(inputText)
      idxes = []
+     select_keyword = []
+     select_question = []
+     final_question = []
 
      $('input:checkbox[name=chkbox]').each(function() {
         if(this.checked){
@@ -82,13 +103,32 @@ function btnClick1() {
            idxes.push(idx);
          }
      })
-     $("#resultWindow").empty()
+
      idxes.forEach(function(idx) {
+        $("#resultWindow").empty()
         console.log(glo_key)
-        select_keyword = glo_key[idx]
+        select_keyword.push(glo_key[idx])
         console.log(select_keyword)
-        select_question = glo_questions[idx]
+        select_question.push(glo_questions[idx])
         console.log(select_question)
-            $("#resultWindow").append(`<tr><td>${select_keyword}</td><td>${select_question}</td></tr>`)
+
+        $("#resultWindow").append(`<tr><td>${select_keyword}</td><td>${select_question}</td></tr>`)
     })
+
+    for (let i =0; i< select_question.length; i++) {
+        questionToString = select_question[i].toString()
+        console.log(questionToString)
+        change = questionToString.replace(select_keyword[i], '__')
+        final_question.push(change)
+    }
+
+        console.log(final_question)
+
+    localStorage.setItem('key',JSON.stringify(glo_key))
+    localStorage.setItem('count',JSON.stringify(glo_count))
+    localStorage.setItem('questions',JSON.stringify(glo_questions))
+    localStorage.setItem('select_keyword',JSON.stringify(select_keyword))
+    localStorage.setItem('select_question',JSON.stringify(select_question))
+    localStorage.setItem('final_question',JSON.stringify(final_question))
+    window.location.href = 'http://127.0.0.1:5000/quiz'
  }
