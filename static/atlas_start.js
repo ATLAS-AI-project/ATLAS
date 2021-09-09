9b08092c498766a0503067463d2992e1e5e6556a
jy1655 <88833517+jy1655@users.noreply.github.com>
2021-09-09 14:40:39 +0900

    Add files via upload

---
+++ static/atlas_start.js
@@ -0,0 +1,3 @@
+window.onload= function() {
+    setTimeout(function() {window.location.replace('http://127.0.0.1:5000/pageSelect')}, 5350)  // 5.35초(5300ms)후 home으로 이동
+}
---
+++ static/pageSelect.js
@@ -0,0 +1,3 @@
+function btnclk(){
+    window.location.href = 'http://127.0.0.1:5000/home'
+}
