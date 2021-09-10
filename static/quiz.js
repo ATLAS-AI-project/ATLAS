92470d8ae767dfd47cc3ca96185b902b2ff8956e
JaeYoung Hwang <88833517+jy1655@users.noreply.github.com>
2021-09-10 13:33:15 +0900

    Update quiz.js
    filename = document.getElementById("title_print").value; 추가 
    id = title_print 에 입력된 텍스트가 다운로드되는 텍스트파일의 제목이 됩니다.

--- static/quiz.js
+++ static/quiz.js
@@ -29,10 +29,11 @@ function homebtn() {
 }
 
 function savebtn() {
+  filename = document.getElementById("title_print").value;
   quest = final_question.toString().replace(/,/g, '.\n\n\n')
   var save = document.createElement('a');
   save.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(quest));
-  save.setAttribute('download', 'filename');
+  save.setAttribute('download', filename);
   if (document.createEvent) {
         var event = document.createEvent('MouseEvents');
         event.initEvent('click', true, true);
