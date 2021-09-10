c2941205ef11423f6f4a5db66cd04e89d1f55156
JaeYoung Hwang <88833517+jy1655@users.noreply.github.com>
2021-09-10 11:25:56 +0900

    via jy1655
    function homebtn() 기능추가 : 원문삽입 페이지로 되돌아감(localStorage 삭제 기능도 추가)
    function savebtn() 기능추가 : 키워드 맞추기 문제를 텍스트파일로 다운로드 가능하게 하는 버튼 (제목 정하는 기능은 추가 예졍)

--- static/quiz.js
+++ static/quiz.js
@@ -3,10 +3,10 @@ window.onload= function() {
     count = JSON.parse(localStorage.getItem("count"))
     questions = JSON.parse(localStorage.getItem("questions"))
     select_keyword = JSON.parse(localStorage.getItem("select_keyword"))
+    console.log(JSON.parse(localStorage.getItem("select_keyword")))
     select_question = JSON.parse(localStorage.getItem("select_question"))
+    console.log(JSON.parse(localStorage.getItem("select_question")))
     final_question = JSON.parse(localStorage.getItem("final_question"))
-//    localStorage.clear()
-
    $("#testlist").empty()
    for (let i =0; i< final_question.length; i++) {
       let result= final_question[i];
@@ -20,6 +20,22 @@ window.onload= function() {
             <td>정답 : ${keyword}</td>
           </tr>
         </tbody>`)}
+ }
+//console.log(localStorage.clear())로 나중에 데이터를 지워주어야함 <-이거는 브라우저에서 삭제를 안하면 계속남아있음
+
+function homebtn() {
+  console.log(localStorage.clear())
+  window.location.href = 'http://127.0.0.1:5000/home'
 }
 
-//localStorage.clear()로 나중에 데이터를 지워주어야함 (console에서)
+function savebtn() {
+  quest = final_question.toString().replace(/,/g, '.\n\n\n')
+  var save = document.createElement('a');
+  save.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(quest));
+  save.setAttribute('download', 'filename');
+  if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        save.dispatchEvent(event);
+    }
+  }
