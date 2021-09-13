f1aced3e0e16e62144268f122ab3a8d763906c68
JaeYoung Hwang <88833517+jy1655@users.noreply.github.com>
2021-09-13 17:33:55 +0900

    Update grades.js
    한줄짜리 코드 간편화

--- static/grades.js
+++ static/grades.js
@@ -8,6 +8,4 @@ function listbtn() {
   window.location.href = 'http://127.0.0.1:5000/testlet_t'
 }
 
-function delbtn(){
-    console.log(localStorage.clear())
-}
+const delbtn = () => console.log(localStorage.clear())
