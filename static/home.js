b5dff02499b4f6f0480be1d52e0913a32893951f
Kim, Che-Pill <82295573+garlicvread@users.noreply.github.com>
2021-09-15 20:16:50 +0900

    Add files via upload

--- static/home.js
+++ static/home.js
@@ -228,3 +228,7 @@ function listbtn() {
   console.log(localStorage.clear())
   window.location.href = 'http://127.0.0.1:5000/testlet_t'
 }
+
+function gradebtn() {
+    window.location.href = 'http://127.0.0.1:5000/grades'
+}
