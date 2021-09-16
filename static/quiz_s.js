a3d7204dd15334125f731ff3b14fec108b57c756
JaeYoung Hwang <88833517+jy1655@users.noreply.github.com>
2021-09-16 17:44:04 +0900

    Update quiz_s.js
    x문제중 x 문제 맞았습니다! 뜨고 다시 문제 선택화면으로 돌아가는 기능까지 구현

--- static/quiz_s.js
+++ static/quiz_s.js
@@ -14,9 +14,38 @@ window.onload= function() {
           console.log(typeof(JSON.parse(list["blank_q"])))
           for(let i=0; i<JSON.parse(list["blank_q"]).length; i++){
             $("#list").append(`
-            <ul>문제${i+1}. ${JSON.parse(list["blank_q"])[i]} <br> <input type='text' name='answer' /></ul>
+            <ul>문제${i+1}. ${JSON.parse(list["blank_q"])[i]} <br> <input type='text' name='answer' id="answer${i}" /></ul>
       `)
           }
     }
+})
+}
+
+function saveanswerbtn() {
+  num = JSON.parse(localStorage.getItem("num"))
+  score= 0
+  wrong= []
+  $.ajax({
+        type: "GET",
+        url: "/list",
+        data: {},
+        async:true,
+        success: function(response){
+          console.log(response)
+          response [{},{},{},{}]
+          let list = response[num];
+          for(let i=0; i<JSON.parse(list["answer"]).length; i++){
+            let txObj = document.getElementById("answer"+i).value;
+            console.log(JSON.parse(list["answer"])[i])
+            console.log(txObj)
+            if(JSON.parse(list["answer"])[i] == txObj){
+              score= score+1
+            }
+            console.log(score)
+          }
+            window.location.href = 'http://127.0.0.1:5000/testlet_s'
+            alert((JSON.parse(list["answer"])).length+'문제중 '+score+'개 맞았습니다!')
+            }
+
 })
 }
