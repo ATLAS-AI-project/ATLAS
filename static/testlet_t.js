cb021e4882374bb9a3220b33057a9a52deba438b
JaeYoung Hwang <88833517+jy1655@users.noreply.github.com>
2021-09-17 10:51:37 +0900

    Update testlet_t.js

--- static/testlet_t.js
+++ static/testlet_t.js
@@ -1,3 +1,23 @@
+window.onload= function() {
+  $.ajax({
+        type: "GET",
+        url: "/list",
+        data: {},
+        async:true,
+        success: function(response){
+          console.log(response)
+          $("#list").empty()
+          response [{},{},{},{}]
+          for(let i=0; i<response.length; i++){
+            let list = response[i];
+            $("#list").append(`
+            <ul>문제지 ${list["index"]}. : ${list["blank_q_name"]}<input type='radio' name='list' /></ul>
+      `)
+          }
+    }
+})
+}
+
 function homebtn() {
   console.log(localStorage.clear())
   window.location.href = 'http://127.0.0.1:5000/home'
