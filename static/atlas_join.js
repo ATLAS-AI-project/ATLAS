40efa86ffceba6654397c3fdff0d6bdf260760ed
JaeYoung Hwang <88833517+jy1655@users.noreply.github.com>
2021-09-23 16:23:42 +0900

    Update atlas_join.js
    가입기능

--- static/atlas_join.js
+++ static/atlas_join.js
@@ -1,10 +1,33 @@
-// $("#month_value option:selected").val();
-
-// $("#day_value option:selected").val();
-
-// $("#teacher_or_student option:selected").val();
-
 function join_submit() {
-    if
-    window.location.href = 'http://127.0.0.1:5000/pageSelect'
+  const id = document.getElementById('id_to_join').value;
+  const password = document.getElementById('pwd_to_join').value;
+  const passwordchk = document.getElementById('pwd_doublecheck').value;
+  if(password != passwordchk){
+    alert('비밀번호가 일치 하지 않습니다.');
+    return false;
+    }
+  const user_name = document.getElementById('user_name').value;
+  const school_name = document.getElementById('school_name').value;
+  const year_born = document.getElementById('year_born').value;
+  const month_born = document.getElementById('month_born').value;
+  const day_born = document.getElementById('day_born').value;
+  const teacher_or_student = document.getElementById('teacher_or_student').value;
+  console.log(id, password, passwordchk , month_born, day_born, teacher_or_student)
+
+  $.ajax({
+        type: "POST",
+        url: "/join",
+        data: {id : id,
+           password : password,
+           user_name : user_name,
+           school_name : school_name,
+           year_born : year_born,
+           month_born : month_born,
+           day_born : day_born,
+           teacher_or_student : teacher_or_student},
+        async:true,
+        success: function(){
+          alert('가입 완료!')
+        }
+})
 }
