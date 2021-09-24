47e7f63a76514a8ba0b73e4726130ae7128e515b
JaeYoung Hwang <88833517+jy1655@users.noreply.github.com>
2021-09-24 11:23:40 +0900

    Update home.js

--- static/home.js
+++ static/home.js
@@ -1,12 +1,9 @@
 window.onload= function() {
-  if (typeof localStorage.getItem('session') == 'undefined'){
-    alert('권한이 없습니다')
-    window.location.href = 'http://127.0.0.1:5000/login'
+  if (typeof localStorage.getItem('session') == 1){
+    return 'ok'
   }else if (localStorage.getItem('session') == 2 || localStorage.getItem('session') == 3){
     alert('권한이 없습니다')
     window.location.href = 'http://127.0.0.1:5000/testlet_s'
-  }else if (localStorage.getItem('session') == 1){
-    return 'ok'
   }else{alert('권한이 없습니다')
     window.location.href = 'http://127.0.0.1:5000/login'}
   }
