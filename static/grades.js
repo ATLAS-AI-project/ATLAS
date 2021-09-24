19c07dee197a472d7beb1ec84b92c0840a758737
JaeYoung Hwang <88833517+jy1655@users.noreply.github.com>
2021-09-24 10:47:42 +0900

    Update grades.js

--- static/grades.js
+++ static/grades.js
@@ -1,13 +1,10 @@
 function homebtn() {
-  console.log(localStorage.clear())
   window.location.href = 'http://127.0.0.1:5000/home'
 }
 
 function listbtn() {
-  console.log(localStorage.clear())
   window.location.href = 'http://127.0.0.1:5000/testlet_t'
 }
 
 function delbtn(){
-    console.log(localStorage.clear())
 }
